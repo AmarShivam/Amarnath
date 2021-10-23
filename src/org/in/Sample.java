5f4d6fea518dae3c8b4becb0f43f050275613bf4
AmarShivam <amaranushya@gmail.com>
2021-10-23 12:33:20 +0530

    Boy

--- src/org/in/Sample.java
+++ src/org/in/Sample.java
@@ -8,10 +8,14 @@ System.out.println("id is 123");
 	private void num() {
 System.out.println("number is 9566");
 }
+	private void mobile() {
+System.out.println("mobile is nokia");
+	}
 	
 	public static void main(String[] args) {
 		Sample s= new Sample();
 		s.id();
 		s.num();
+		s.mobile();
 	}
 }
