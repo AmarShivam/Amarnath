1d487490cdaf5a24e1cce8e4d48ae524a9ad3a03
AmarShivam <amaranushya@gmail.com>
2021-10-23 12:22:38 +0530

    Fish

--- src/org/in/Sample.java
+++ src/org/in/Sample.java
@@ -8,10 +8,15 @@ System.out.println("id is 123");
 	private void num() {
 System.out.println("number is 9566");
 }
+	private void area() {
+System.out.println("area is nanganallur");
+	}
 	
 	public static void main(String[] args) {
 		Sample s= new Sample();
 		s.id();
 		s.num();
+		s.area();
+		
 	}
 }
