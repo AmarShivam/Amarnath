471d6aa5fc5db38aec1f8e7030f9f171c5885947
AmarShivam <amaranushya@gmail.com>
2021-10-23 12:00:45 +0530

    Amar

---
+++ src/org/in/Sample.java
@@ -0,0 +1,17 @@
+package org.in;
+
+public class Sample {
+	
+	private void id() {
+System.out.println("id is 123");
+	}
+	private void num() {
+System.out.println("number is 9566");
+}
+	
+	public static void main(String[] args) {
+		Sample s= new Sample();
+		s.id();
+		s.num();
+	}
+}
